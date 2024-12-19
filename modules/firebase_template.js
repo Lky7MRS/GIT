81252d360e3addb1df95e12360f98a1f1d8f79b2
Lky7MRS <lky7mrs@gmail.com>
2024-12-19 15:45:20 +0100

    Create firebase_template.js

---
+++ modules/firebase_template.js
@@ -0,0 +1,27 @@
+// modules/firebase.js
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL:
+    "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "1",
+  appId: "",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Get the database reference
+const db = getDatabase(app);
+const auth = getAuth(app);
+
+console.log(firebaseConfig, app, db);
+
+export { db, auth };
